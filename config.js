4b739b01f891a16cd9c133f19bee35c121286cb9
ThinSquirrel <90573839+ThinSquirrel@users.noreply.github.com>
2022-02-18 08:59:41 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBtEDmMTS8ljPUfjyUzkCAex28nZV16mlU",
+    authDomain: "project85-3740d.firebaseapp.com",
+    projectId: "project85-3740d",
+    storageBucket: "project85-3740d.appspot.com",
+    messagingSenderId: "883416937578",
+    appId: "1:883416937578:web:e6e6b53cb8236b433f8613"
+  };
